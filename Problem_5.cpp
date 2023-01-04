9197e194916ce662e6e9acfd8c80e96e079ec32e
Anath <anath.tabaczka@protonmail.com>
2023-01-04 02:21:58 +0100

    Smallest multiple

--- Problem_5.cpp
+++ Problem_5.cpp
@@ -3,6 +3,10 @@
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 */
+/*!
+ *\author Anath
+ *\date January 2023
+ */
 
 #include <iostream>
 int main()
