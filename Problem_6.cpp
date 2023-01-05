104f704b0b56b4890e0b005d60cf3f7d3f902eff
Anath <anath.tabaczka@protonmail.com>
2023-01-05 01:25:43 +0100

    Sum square difference

--- Problem_6.cpp
+++ Problem_6.cpp
@@ -1,6 +1,5 @@
 /*
 The sum of the squares of the first ten natural numbers is,
-
 1^2+2^2+...+10^2=385
 The square of the sum of the first ten natural numbers is,
 (1+2+...+10)^2=55^2=3025
@@ -21,7 +20,6 @@ unsigned int sumOfTheSquares(int quantity)
     {
         sum = sum + (i * i);
     }
-
     return sum;
 }
 unsigned int squareOfTheSum(int quantity)
